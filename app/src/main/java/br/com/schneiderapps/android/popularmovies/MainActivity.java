38c071583a97b61392a9c271843fc421f661208d
Matheus Schneider <matheus.ssm@gmail.com>
2018-10-16 21:42:26 -0300

    Add MainActivity initial code

--- app/src/main/java/br/com/schneiderapps/android/popularmovies/MainActivity.java
+++ app/src/main/java/br/com/schneiderapps/android/popularmovies/MainActivity.java
@@ -1,13 +1,92 @@
 package br.com.schneiderapps.android.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MovieAdapter.MovieAdapterOnClickHandler{
+
+    private RecyclerView mRecyclerView;
+    private MovieAdapter mMovieAdapter;
+
+    private TextView mErrorMessageDisplay;
+
+    private ProgressBar mLoadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recyclerview_movies);
+
+        mErrorMessageDisplay = findViewById(R.id.tv_error_message_display);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, GridLayoutManager.VERTICAL);
+
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+
+        mLoadingIndicator = findViewById(R.id.pb_loading_indicator);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mMovieAdapter = new MovieAdapter(this);
+
+        mRecyclerView.setAdapter(mMovieAdapter);
+
+        loadMoviesData();
+    }
+
+    private void loadMoviesData(){
+
+    }
+
+    private void showMoviesDataView() {
+
+        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private void showErrorMessage() {
+
+        mRecyclerView.setVisibility(View.INVISIBLE);
+
+        mErrorMessageDisplay.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.movies, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+//        if (id == R.id.action_refresh) {
+//            mForecastAdapter.setWeatherData(null);
+//            loadMoviesData();
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(String movieClicked) {
+
     }
 }
