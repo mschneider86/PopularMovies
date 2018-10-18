efcce4ba94582c5cab0a93dd3ed705d3e2298604
Matheus Schneider <matheus.ssm@gmail.com>
2018-10-17 21:14:04 -0300

    update MovieAdapter initial code

--- app/src/main/java/br/com/schneiderapps/android/popularmovies/MovieAdapter.java
+++ app/src/main/java/br/com/schneiderapps/android/popularmovies/MovieAdapter.java
@@ -1,45 +1,65 @@
 package br.com.schneiderapps.android.popularmovies;
 
+import android.content.Context;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
+import java.util.List;
+
+import br.com.schneiderapps.android.popularmovies.pojo.Movie;
+import br.com.schneiderapps.android.popularmovies.utilities.NetworkUtils;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieAdapterViewHolder>{
 
+    private List<Movie> mMovieList;
+    private Context mContext;
     private final MovieAdapterOnClickHandler mClickHandler;
 
     public interface MovieAdapterOnClickHandler{
         void onClick(String movieClicked);
     }
 
-    public MovieAdapter(MovieAdapterOnClickHandler clickHandler) {
+    public MovieAdapter(Context context, List<Movie> movieList, MovieAdapterOnClickHandler clickHandler) {
+
+        mContext = context;
+        mMovieList = movieList;
         mClickHandler = clickHandler;
 
     }
 
     @NonNull
     @Override
-    public MovieAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public MovieAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cardview_item_movie, viewGroup, false);
+
+        return new MovieAdapterViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MovieAdapterViewHolder movieAdapterViewHolder, int i) {
-
+    public void onBindViewHolder(@NonNull MovieAdapterViewHolder movieAdapterViewHolder, int position) {
+        Uri moviePosterUri = NetworkUtils.buildImageUri(mMovieList.get(position).getPosterPath());
+        Picasso.with(mContext).load(moviePosterUri).into(movieAdapterViewHolder.mImageView);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mMovieList.size();
     }
 
     public class MovieAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
-        public MovieAdapterViewHolder(View view) {
-            super(view);
+        ImageView mImageView;
 
-            view.setOnClickListener(this);
+        public MovieAdapterViewHolder(View itemView) {
+            super(itemView);
+
+            mImageView = itemView.findViewById(R.id.iv_movie_image);
+            itemView.setOnClickListener(this);
 
         }
 
