f8367d371483ea374eaa2f3f8f3b6c18302a2000
Matheus Schneider <matheus.ssm@gmail.com>
2018-10-17 21:03:59 -0300

    add Movie pojo class

---
+++ app/src/main/java/br/com/schneiderapps/android/popularmovies/pojo/Movie.java
@@ -0,0 +1,60 @@
+package br.com.schneiderapps.android.popularmovies.pojo;
+
+public class Movie {
+
+    private int id;
+    private String originalTitle;
+    private String overview;
+    private String posterPath;
+    private String releaseDate;
+    private double voteAverage;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+}
+
