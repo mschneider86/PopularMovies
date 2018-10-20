0fb6eb522e9d84bc6b4113e57c9d8a6062ee6f43
Matheus Schneider <matheus.ssm@gmail.com>
2018-10-19 21:25:25 -0300

    add methods to form url, check if the user is online

--- app/src/main/java/br/com/schneiderapps/android/popularmovies/utilities/NetworkUtils.java
+++ app/src/main/java/br/com/schneiderapps/android/popularmovies/utilities/NetworkUtils.java
@@ -1,6 +1,16 @@
 package br.com.schneiderapps.android.popularmovies.utilities;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
 
 
 public class NetworkUtils {
@@ -8,14 +18,36 @@ public class NetworkUtils {
     private static final String THE_MOVIE_DB_URL = "http://api.themoviedb.org/3/movie/";
     private static final String IMAGE_BASE_URL = "http://image.tmdb.org";
 
-    //TODO remove api key before commit
     private static final String API_KEY_QUERY_PARAM = "api_key";
+    //TODO remove api key before commit
+    private static final String MOVIE_DB_API_KEY = "INSERT_YOUR_API_KEY_HERE";
+
 
     private static final String T_PATH = "t";
     private static final String P_PATH = "p";
-    private static final String IMAGE_SIZE_PATH = "w185";
+    private static final String IMAGE_SIZE_PATH = "w342";
     public static final String POPULAR_MOVIES_PATH = "popular";
     public static final String TOP_RATED__MOVIES_PATH = "top_rated";
+
+    public static URL buildUrl(String... apiParams) {
+        Uri.Builder uriBuilder = Uri.parse(THE_MOVIE_DB_URL).buildUpon();
+
+
+        for (String p: apiParams) {
+            uriBuilder.appendPath(p);
+        }
+
+        Uri builtUri = uriBuilder.appendQueryParameter(API_KEY_QUERY_PARAM, MOVIE_DB_API_KEY).build();
+        URL url = null;
+
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
 
     public static Uri buildImageUri(String moviePosterUrl) {
         Uri builtUri = Uri.parse(IMAGE_BASE_URL).buildUpon()
@@ -26,5 +58,37 @@ public class NetworkUtils {
                 .build();
 
         return builtUri;
+    }
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+    public static boolean isOnline(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = connectivityManager.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnected();
     }
 }
