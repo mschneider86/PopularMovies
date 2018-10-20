ac986d1a2e018f91787dd2a5e6da9ec291e7c7f3
Matheus Schneider <matheus.ssm@gmail.com>
2018-10-19 21:31:14 -0300

    update load of movies, add strings, menu itens and icons

--- app/src/main/java/br/com/schneiderapps/android/popularmovies/MainActivity.java
+++ app/src/main/java/br/com/schneiderapps/android/popularmovies/MainActivity.java
@@ -1,15 +1,28 @@
 package br.com.schneiderapps.android.popularmovies;
 
+import android.os.AsyncTask;
+import android.os.Parcelable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.schneiderapps.android.popularmovies.pojo.Movie;
+import br.com.schneiderapps.android.popularmovies.utilities.JsonUtils;
+import br.com.schneiderapps.android.popularmovies.utilities.NetworkUtils;
 
 public class MainActivity extends AppCompatActivity implements MovieAdapter.MovieAdapterOnClickHandler{
 
@@ -20,6 +33,8 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
 
     private ProgressBar mLoadingIndicator;
 
+    List<Movie> mMovieList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +44,9 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
 
         mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
 
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, GridLayoutManager.VERTICAL);
+        //Poster width in px, same as the size informed in the query (w342), just a random value
+        int posterWidth = 342;
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, calculateBestSpanCount(posterWidth));
 
         mRecyclerView.setLayoutManager(gridLayoutManager);
 
@@ -37,15 +54,30 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
 
         mRecyclerView.setHasFixedSize(true);
 
-       // mMovieAdapter = new MovieAdapter(this);
+        mMovieAdapter = new MovieAdapter(this, this);
 
         mRecyclerView.setAdapter(mMovieAdapter);
 
-        loadMoviesData();
+        mMovieList = new ArrayList<>();
+
+        if(savedInstanceState == null || !savedInstanceState.containsKey("movies")){
+            loadMoviesData(NetworkUtils.POPULAR_MOVIES_PATH);
+        }else{
+            mMovieList = savedInstanceState.getParcelableArrayList("movies");
+            mMovieAdapter.setMoviesData(mMovieList);
+        }
+
     }
 
-    private void loadMoviesData(){
+    private void loadMoviesData(String sortCriteria){
 
+        if(NetworkUtils.isOnline(this)) {
+            showMoviesDataView();
+
+            new FetchPopularMoviesTask().execute(sortCriteria);
+
+        }else
+            showErrorMessage(getResources().getString(R.string.error_message_network));
     }
 
     private void showMoviesDataView() {
@@ -55,11 +87,26 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
         mRecyclerView.setVisibility(View.VISIBLE);
     }
 
-    private void showErrorMessage() {
+    private void showErrorMessage(String message) {
 
         mRecyclerView.setVisibility(View.INVISIBLE);
 
+        mErrorMessageDisplay.setText(message);
         mErrorMessageDisplay.setVisibility(View.VISIBLE);
+    }
+
+    private int calculateBestSpanCount(int posterWidth) {
+        Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        display.getMetrics(outMetrics);
+        float screenWidth = outMetrics.widthPixels;
+        return Math.round(screenWidth / posterWidth);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putParcelableArrayList("movies", (ArrayList<? extends Parcelable>) mMovieList);
+        super.onSaveInstanceState(outState);
     }
 
     @Override
@@ -76,17 +123,72 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-//        if (id == R.id.action_refresh) {
-//            mForecastAdapter.setWeatherData(null);
-//            loadMoviesData();
-//            return true;
-//        }
+        if (id == R.id.action_sort_by_popular_movies) {
+            if (!item.isChecked())
+                item.setChecked(true);
+
+            loadMoviesData(NetworkUtils.POPULAR_MOVIES_PATH);
+            return true;
+
+        } else if (id == R.id.action_sort_by_top_rated) {
+            if (!item.isChecked())
+                item.setChecked(true);
+
+            loadMoviesData(NetworkUtils.TOP_RATED__MOVIES_PATH);
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
 
     @Override
-    public void onClick(String movieClicked) {
+    public void onClick(Movie selectedMovie) {
+
+    }
+
+    private class FetchPopularMoviesTask extends AsyncTask<String, Void, List<Movie>> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected List<Movie> doInBackground(String... params) {
+
+            if (params.length == 0) {
+                return null;
+            }
+
+            String sortCriteria = params[0];
+            URL movieRequestUrl = NetworkUtils.buildUrl(sortCriteria);
+
+            try {
+                String jsonBuiltURLResponse = NetworkUtils.getResponseFromHttpUrl(movieRequestUrl);
+
+                return JsonUtils.getMovieListFromJson(jsonBuiltURLResponse);
+            } catch (Exception e) {
+                e.printStackTrace();
+
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(List<Movie> movieList) {
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+            if (movieList != null) {
+                showMoviesDataView();
+
+                mMovieList = movieList;
+
+                mMovieAdapter.setMoviesData(mMovieList);
+            } else {
+                showErrorMessage(getResources().getString(R.string.error_message_default));
+            }
+
+        }
 
     }
 }
